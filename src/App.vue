029040fbc1990993118cafbd5e2b1ec4e979eb18
tookbra <tookbra@gmail.com>
2019-05-11 21:06:50 +0800

    :sparkles: add locale

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view />
+    </div>
+  </a-locale-provider>
 </template>
 
 <script>
+import zhCN from "ant-design-vue/lib/locale-provider/zh_CN";
 import { deviceEnquire, DEVICE_TYPE } from "@/libs/device";
 export default {
+  data() {
+    return {
+      locale: zhCN
+    };
+  },
   mounted() {
     const { $store } = this;
     deviceEnquire(deviceType => {
