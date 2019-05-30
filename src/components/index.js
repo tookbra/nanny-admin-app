7fcec4d1958cbe57caba1cc959bc032a8774fa91
tookbra <tookbra@outlook.com>
2019-05-30 23:27:03 +0800

    :sparkles: add IconSelector component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import BasicLayout from "@/components/layout/basic-layout";
 import STable from "@/components/table";
 import tableMenu from "@/components/tableMenu/index";
 import PageLoading from "@/components/PageLoading";
+import IconSelector from '@/components/IconSelector'
 
-export { ExceptionPage, BasicLayout, STable, tableMenu, PageLoading };
+export { ExceptionPage, BasicLayout, STable, tableMenu, PageLoading, IconSelector };
